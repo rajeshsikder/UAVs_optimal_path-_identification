fd03c6c97968e1387b0138b61e80c1652c473f1a
Rajesh Sikder <60202857+rajeshsikder@users.noreply.github.com>
2020-06-16 18:10:59 -0400

    Add files via upload

---
+++ ACO Code/RouletteWheelSelection.m
@@ -0,0 +1,11 @@
+
+
+function j=RouletteWheelSelection(P)
+
+    r=rand;
+    
+    C=cumsum(P);
+    
+    j=find(r<=C,1,'first');
+
+end
---
+++ ACO Code/TourLength.m
@@ -0,0 +1,14 @@
+
+
+function L=TourLength(tour,model)
+
+    n=numel(tour);
+
+    tour=[tour tour(1)];
+    
+    L=0;
+    for i=1:n
+        L=L+model.D(tour(i),tour(i+1));
+    end
+
+end
